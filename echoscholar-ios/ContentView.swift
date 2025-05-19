a8dcbbc29f23a554bb69c4369f4c4db1e85213f0
Bibin Joseph <bibintomj@gmail.com>
2025-05-19 13:10:24 -0400

    Initial Commit

---
+++ echoscholar-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echoscholar-ios
+//
+//  Created by Bibin Joseph on 2025-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echoscholar-ios/echoscholar_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoscholar_iosApp.swift
+//  echoscholar-ios
+//
+//  Created by Bibin Joseph on 2025-05-19.
+//
+
+import SwiftUI
+
+@main
+struct echoscholar_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
